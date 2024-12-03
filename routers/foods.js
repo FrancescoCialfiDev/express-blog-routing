34c342aa4a4112f913a0b92f622cb2930515a239
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-03 16:46:06 +0100

    formatted text

--- routers/foods.js
+++ routers/foods.js
@@ -4,21 +4,24 @@ const express = require("express");
 // - Utilizziamo il metodo express per gestire le rotte in modo modulare.
 const foodsRouter = express.Router();
 
-// READ:
+// STEP 2 
+// - Creiamo le richieste http con i vari metodi (GET,POST,PUT,PATCH,DELETE)
+
+// READ
 foodsRouter.get('/', (req, res) => res.send('Lista intera alimenti')); // Richiesta get generica 
 foodsRouter.get('/:id', (req, res) => res.send(`Lista singolo alimento`)); // Richiesta get specifica
 
-// CREATE:
+// CREATE
 foodsRouter.post('/', (req, res) => res.send('Creazione nuovo alimento')); // Creazione di un nuovo alimento
 
-// UPDATE:
+// UPDATE
 foodsRouter.put('/:id', (req, res) => res.send(`Aggiornamento alimento`)); // Aggiornamento completo di un alimento
 
-// PATCH:
+// PATCH
 foodsRouter.patch('/:id', (req, res) => res.send(`Aggiornamento parziale alimento`)); // Aggiornamento parziale di un alimento
 
-// DELETE:
+// DELETE
 foodsRouter.delete('/:id', (req, res) => res.send(`Eliminazione alimento`)); // Eliminazione di un alimento
 
-// Esportazione del modulo.
+// Esportazione del modulo
 module.exports = foodsRouter;
